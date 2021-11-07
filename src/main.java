b30f38c648cc8d69ad3ecc89bc5cf481a3d277a7
Moks <gutamax2@mail.ru>
2021-11-07 19:27:10 +0500

    calculate by miles

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int price = 13900;
+        int spent = 20;
+
+        int miles = price/spent;
+        System.out.println(miles);
+    }
+}
