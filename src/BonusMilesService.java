61a2a9e2b4c50b720894f929af97a84f95b6fae0
Moks <gutamax2@mail.ru>
2021-11-09 19:02:24 +0500

    Modern Miles

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,7 @@
 public class BonusMilesService {
+    public int calculate (int cost){
+        int spent = 20;
+        int miles = cost/spent;
+        return miles;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,8 @@
 public class main {
     public static void main(String[] args) {
-        int price = 13900;
-        int spent = 20;
-
-        int miles = price/spent;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
